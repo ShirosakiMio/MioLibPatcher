8dc10d17f0d1c3124849e3c03dd5b648901c9c63
ShirosakiMio <852468399@qq.com>
2026-09-09 12:17:10 +0800

    feat: TTSTransformer 增加 ttsDisable 门控，默认不再禁用游戏复述 (#8)

--- src/main/java/com/mio/libpatcher/transformer/TTSTransformer.java
+++ src/main/java/com/mio/libpatcher/transformer/TTSTransformer.java
@@ -4,6 +4,10 @@ import javassist.CannotCompileException;
 import javassist.CtClass;
 import javassist.CtMethod;
 
+/**
+ * 原本用于在缺失 flite 引擎时禁用复述功能；启动器提供 libflite 桥接安卓 TTS 后不再需要，
+ * 补丁默认关闭，仅当显式指定 -Dmiolibpatcher.ttsDisable=true 时保留旧行为。
+ */
 public class TTSTransformer implements BaseTransformer {
     @Override
     public String getTargetClassName() {
@@ -12,6 +16,9 @@ public class TTSTransformer implements BaseTransformer {
 
     @Override
     public void transform(CtClass clazz) throws Throwable {
+        if (!Boolean.parseBoolean(System.getProperty("miolibpatcher.ttsDisable", "false"))) {
+            return;
+        }
         CtMethod method = clazz.getDeclaredMethod("getNarrator");
         try {
             method.setBody("{ return new com.mojang.text2speech.NarratorDummy(); }");
